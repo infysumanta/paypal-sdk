3357daae3c8194dec2908cb173024ccb21ed6903
Sumanta <sumantablog@gmail.com>
2024-02-18 00:24:40 +0530

    feature : update npmignore files and index module

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   dts: true,
-
   entry: ['src/index.ts'],
   format: ['esm'],
   sourcemap: true,
   minify: true,
+  bundle: true,
   target: 'esnext',
   outDir: 'dist',
 })
